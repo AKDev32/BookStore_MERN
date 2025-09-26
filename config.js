0bca7631a4142294210777d9cb25091e6a507d15
AKDev32 <amankumar005562@gmail.com>
2025-09-26 23:39:40 +0530

    Added Routes

---
+++ config.js
@@ -0,0 +1,6 @@
+export const PORT = 5555
+
+export const mongoDBURL = 'mongodb+srv://amanku6936_db_user:CdPZhaBvgBKdAFfP@bookstoremern.mvmte21.mongodb.net/books-collections?retryWrites=true&w=majority&appName=BOOKSTOREMERN'
+
+// CdPZhaBvgBKdAFfP
+// Current IP Address (49.156.86.73/32) added!
